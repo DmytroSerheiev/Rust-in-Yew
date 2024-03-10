81ea1423e4fa5efda814cb362dec912f628a0a78
Dmytro Serheiev <DSerSmart@gmail.com>
2024-03-10 16:17:43 +0200

    add youtube

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,27 @@ use yew::prelude::*;
 #[function_component(App)]
 fn app() -> Html {
     html! {
-        <h1>{ "Hello World" }</h1>
+        <>
+            <h1>{ "RustConf Explorer" }</h1>
+            <div>
+                <h3>{"Videos to watch"}</h3>
+                <p>{ "John Doe: Building and breaking things" }</p>
+                <p>{ "Jane Smith: The development process" }</p>
+                <p>{ "Matt Miller: The Web 7.0" }</p>
+                <p>{ "Tom Jerry: Mouseless development" }</p>
+            </div>
+            <div>
+                <h3>{ "John Doe: Building and breaking things" }</h3>
+                // Вставте код для вбудованого відеоплеєра YouTube
+                <iframe
+                    width="560"
+                    height="315"
+                    src="https://www.youtube.com/embed/k-gVqe3HgwY"
+                    frameborder="0"
+                    allowfullscreen=true
+                ></iframe>
+            </div>
+        </>
     }
 }
 
--- target/wasm-bindgen/debug/yew-app.js
+++ target/wasm-bindgen/debug/yew-app.js
@@ -630,11 +630,11 @@ function __wbg_get_imports() {
     imports.wbg.__wbindgen_throw = function(arg0, arg1) {
         throw new Error(getStringFromWasm0(arg0, arg1));
     };
-    imports.wbg.__wbindgen_closure_wrapper789 = function(arg0, arg1, arg2) {
+    imports.wbg.__wbindgen_closure_wrapper796 = function(arg0, arg1, arg2) {
         const ret = makeClosure(arg0, arg1, 99, __wbg_adapter_20);
         return addHeapObject(ret);
     };
-    imports.wbg.__wbindgen_closure_wrapper3059 = function(arg0, arg1, arg2) {
+    imports.wbg.__wbindgen_closure_wrapper3075 = function(arg0, arg1, arg2) {
         const ret = makeMutClosure(arg0, arg1, 233, __wbg_adapter_23);
         return addHeapObject(ret);
     };
